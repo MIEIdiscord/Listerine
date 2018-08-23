24596a4ae0acb4d12f342f07ab51ae1874c9cf09
José FerreIra <jose.filipe.matos.ferreira@gmail.com>
2018-08-23 22:59:25 +0100

    add simple ping pong bot and instructions for setup

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 TODO: Discord BOT
 
+## Getting Started
+
+go to [config.exs](config/config.exs) and insert your token
+
+```shell
+mix deps.get
+```
+
+```shell
+mix compile
+```
+
+```shell
+iex -S mix
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
--- mix.exs
+++ mix.exs
@@ -1,28 +1,25 @@
-defmodule Listerine.MixProject do
+defmodule Example.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :listerine,
+      app: :Listerine,
       version: "0.1.0",
-      elixir: "~> 1.7",
-      start_permanent: Mix.env() == :prod,
+      elixir: "~> 1.5",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      mod: {Listerine, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:coxir, git: "https://github.com/satom99/coxir.git"}
     ]
   end
