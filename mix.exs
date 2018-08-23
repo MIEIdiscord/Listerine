43b30cda1a676acac433d235f9457f058c5e970d
Mendess <pedro.mendes.26@gmail.com>
2018-08-23 09:53:18 +0100

    Add coxir dependency

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Listerine.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:coxir, git: "https://github.com/satom99/coxir.git"}
     ]
   end
 end
