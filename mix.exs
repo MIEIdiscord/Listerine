b5bc70dbaad62cca52e66ecca1b9c5420b2d6366
José FerreIra <jose.filipe.matos.ferreira@gmail.com>
2018-08-24 11:14:19 +0100

    Remove ambiguity in mix.exs, lowercase the name app and reverse name change

--- mix.exs
+++ mix.exs
@@ -1,13 +1,13 @@
-defmodule Example.Mixfile do
+defmodule Listerine.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :Listerine,
+      app: :listerine,
       version: "0.1.0",
       elixir: "~> 1.7",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_embedded: {Mix.env == :prod},
+      start_permanent: {Mix.env == :prod},
       deps: deps()
     ]
   end
