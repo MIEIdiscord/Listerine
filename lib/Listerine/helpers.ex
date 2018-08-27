f9dcbadfb29b38420cfc86a10e6fecc974e7810d
hitlergit <38398296+hitlergit@users.noreply.github.com>
2018-08-27 16:49:01 +0100

    Add study command (#8)

---
+++ lib/Listerine/helpers.ex
@@ -0,0 +1,26 @@
+defmodule Listerine.Helpers do
+  use Coxir
+
+  def add_role_list(message, role_names) do
+    guild = message.channel.guild_id
+    roles = Guild.get_roles(guild)
+    if role_names != [] do
+      [head | tail] = role_names
+      add_role_name(head, message, roles, guild)
+      add_role_list(message, tail)
+    end
+  end
+
+  def add_role_name(_name, message, [], _guild) do
+    Message.reply(message, "Role does not exist")
+  end
+
+  def add_role_name(name, message, [head | tail], guild) do 
+    member = Guild.get_member(guild, message.author.id)
+    if head.name == name do 
+      Member.add_role(member, head.id)
+    else
+      add_role_name(name, message, tail, guild)
+    end
+  end 
+end 
