15848159ad4be49a72c2e5a7f706b0261d421c41
Hitler <a85504@alunos.uminho.pt>
2018-08-30 17:06:56 +0100

    Add parenthesis to intersect and move unwords to helpers.ex

--- lib/Listerine/helpers.ex
+++ lib/Listerine/helpers.ex
@@ -1,5 +1,8 @@
 defmodule Listerine.Helpers do
   use Coxir
 
-  def intersect(a, b), do: a -- a -- b
+  def intersect(a, b), do: a -- (a -- b)
+
+  def unwords(words), do: Enum.reduce(words, fn x, a -> a <> " " <> x end)
+
 end
