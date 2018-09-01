540da55caf38c41b4dcc825626f69b8f1734fda3
José FerreIra <jose.filipe.matos.ferreira@gmail.com>
2018-09-01 15:40:27 +0100

    Add parentheses

--- lib/Listerine/helpers.ex
+++ lib/Listerine/helpers.ex
@@ -4,7 +4,7 @@ defmodule Listerine.Helpers do
   def get_guild_icon_url(guild),
     do: "https://cdn.discordapp.com/icons/" <> guild.id <> "/" <> guild.icon <> ".png"
 
-  def intersect(a, b), do: a -- a -- b
+  def intersect(a, b), do: a -- (a -- b)
 
   def unwords(words), do: Enum.reduce(words, fn x, a -> a <> " " <> x end)
 end
