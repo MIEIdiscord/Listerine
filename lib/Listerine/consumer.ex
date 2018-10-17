2fbb563650c88cb0ccc90411f01c43ea833fd67b
Mendess2526 <pedro.mendes.26@gmail.com>
2018-10-17 22:35:20 +0100

    Add automatic voice channels

--- lib/Listerine/consumer.ex
+++ lib/Listerine/consumer.ex
@@ -10,8 +10,7 @@ defmodule Listerine.Consumer do
       footer: %{
         text: "Qualquer dúvida sobre o bot podes usar $man man para saberes o que podes fazer."
       },
-      description:
-      """
+      description: """
       O nosso objetivo é facilitar a vossa passagem neste curso, através de um servidor com todas as cadeiras, materiais e conteúdos para que possam estar sempre a par do que acontece em cada cadeira.
       Temos também uma sala `#geral` onde podemos conversar de uma forma mais informal e um conjunto de `#regras` que devem ser cumpridas e que podem sempre consultar com alguma dúvida que tenham.
       Temos também o nosso bot #{User.get().username} que permite que te juntes às salas das cadeiras com o comando `$study CADEIRA1, CADEIRA2, ...` ou, se preferires, podes-te juntar a todas as cadeiras de um ano com o comando `$study Xano` substituindo o `X` pelo ano que queres.
--- lib/Listerine/voice_channels.ex
+++ lib/Listerine/voice_channels.ex
@@ -1,12 +1,30 @@
 defmodule Listerine.VoiceChannels do
+  use Coxir
 
   def normalize_voice_channels(guild_id) do
     Guild.get(guild_id).channels
     |> Enum.filter(fn x -> x.type == 2 end)
-    |> Enum.filter(fn x -> String.contains(x.name, "Study") end)
+    |> Enum.filter(fn x -> String.contains?(x.name, "Study") end)
     |> Enum.filter(fn x -> length(Channel.get_voice_members(x)) < 1 end)
+    |> (fn l ->
+          cond do
+            length(l) > 1 -> delete_unnecessary_channels(l)
+            length(l) < 1 -> create_new_channels(guild_id)
+            true -> nil
+          end
+        end).()
+  end
+
+  defp delete_unnecessary_channels(channels) do
+    channels
     |> Enum.sort(&(&1.position <= &2.position))
-    |> fn [h | t] -> t end
-    |> Enum.foreach()
+    |> (fn [_h | t] -> t end).()
+    |> Enum.each(&Channel.delete(&1))
+  end
+
+  defp create_new_channels(guild_id) do
+    IO.puts("before: #{length(Guild.get(guild_id).channels)}")
+    Guild.create_channel(Guild.get(guild_id), %{name: "Study", type: 2})
+    IO.puts("after: #{length(Guild.get(guild_id).channels)}")
   end
 end
